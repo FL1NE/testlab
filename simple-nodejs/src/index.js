99e888bfaf84f320f24860fdf1c7ad17f05a62cd
Tomoki "FL1NE" SHISHIKURA <tomoki.shishikura@gmail.com>
2021-02-03 13:41:38 +0900

    Using import to load random

--- simple-nodejs/src/index.js
+++ simple-nodejs/src/index.js
@@ -1,5 +1,6 @@
-// import random from 'random'
-const random = require('random')
+import random from 'random' //"type": "module",
+// const random = require('random')
 
 console.log("Hello World!")
+// console.log(random)
 console.log("Here is your random number[0-100]:" + random.int(0, 100))
