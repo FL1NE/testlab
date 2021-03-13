012d398fc89004f15528649e7a2275de6a8b697c
Tomoki "FL1NE" SHISHIKURA <tomoki.shishikura@gmail.com>
2021-03-13 14:39:54 +0900

    commit

--- SimpleNuxtChatApp/layouts/default.vue
+++ SimpleNuxtChatApp/layouts/default.vue
@@ -1,64 +1,45 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
+    <v-system-bar app>
+      <div>FRONTL1NE SYSTEM1 vX.Y.Z</div>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-square</v-icon>
+      <v-icon>mdi-circle</v-icon>
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+
+    <v-app-bar clipped-left fixed app>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+
+    <v-navigation-drawer clipped app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
       <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
+        <!-- <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> -->
       </v-list>
     </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
+
+    <!-- <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -84,7 +65,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js',
+      title: 'SYSTEM1',
     }
   },
 }
---
+++ SimpleNuxtChatApp/pages/chat.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <!-- aaaaaaa -->
+    <!-- <v-input></v-input> -->
+    <!-- <div>{{ texts }}</div> -->
+    <div v-for="text in texts" :key="text">
+      <v-card color="#385F73" dark>
+        <v-card-title class="headline"> {{ text }} </v-card-title>
+
+        <!-- <v-card-subtitle
+          >Listen to your favorite artists and albums whenever and wherever,
+          online and offline.</v-card-subtitle
+        > -->
+
+        <!-- <v-card-actions>
+          <v-btn text> Listen Now </v-btn>
+        </v-card-actions> -->
+      </v-card>
+      <!-- {{ text }} -->
+    </div>
+
+    <!-- <div>{{ message }}</div> -->
+    <!-- <v-text-field
+      v-model="message"
+      label="Message"
+      hide-details="auto"
+    ></v-text-field> -->
+    <!-- <v-form> -->
+    <v-footer app color="transparent" height="72" inset>
+      <v-text-field
+        v-model="message"
+        :append-outer-icon="message ? 'mdi-send' : 'mdi-send'"
+        filled
+        clear-icon="mdi-close-circle"
+        clearable
+        label="Message"
+        type="text"
+        @click:append-outer="sendMessage"
+        @click:clear="clearMessage"
+        @keyup.enter="sendMessage()"
+      ></v-text-field>
+    </v-footer>
+    <!-- </v-form> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      texts: [],
+      message: '',
+    }
+  },
+
+  methods: {
+    sendMessage() {
+      this.texts.push(this.message)
+      this.clearMessage()
+    },
+    clearMessage() {
+      this.message = ''
+    },
+  },
+}
+</script>
+
+<style scoped></style>
